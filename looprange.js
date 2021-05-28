701b029eff8f6e414c1bdf14ba4e562527eb9e25
oshimaga-24 <84975434+oshimaga-24@users.noreply.github.com>
2021-05-28 19:04:00 +0530

    Add files via upload

---
+++ looprange.js
@@ -0,0 +1,12 @@
+for (var x=20; x<=30; x++) {
+    if (x % 2 === 0) {
+             document.write(x + "is Even");
+             document.write('<br/>');
+             console.log(x + " is even");   
+     }
+     else {
+              document.write(x + "is odd");
+              document.write('<br/>');
+              console.log(x + " is odd");
+     }
+ }
